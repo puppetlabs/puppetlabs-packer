bf7bd4dfe504ef8c4b877ba07a7f16ac53cc11e2
Scott Schneider <sschneider@puppetlabs.com>
2014-07-10 16:02:35 -0700

    Adding Packer templates and kickstart for RedHat 7.0 support

--- manifests/modules/packer/manifests/networking/params.pp
+++ manifests/modules/packer/manifests/networking/params.pp
@@ -13,6 +13,19 @@ class packer::networking::params {
           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
         }
 
+        7.0: {
+          $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
+
+          case $::provisioner {
+            virtualbox: { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
+            vmware:     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
+
+            default: {
+              fail( "Unsupported provisioner: ${::provisioner}" )
+            }
+          }
+        }
+
         default: {
           $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
