5b585ab47c6533ce60c0790fed2c3a13374755c6
Scott Schneider <sschneider@puppetlabs.com>
2014-10-30 13:28:23 -0700

    EL7 has different interface network-names

--- manifests/modules/packer/manifests/networking/params.pp
+++ manifests/modules/packer/manifests/networking/params.pp
@@ -8,6 +8,16 @@ class packer::networking::params {
 
     redhat: {
       case $::operatingsystemrelease {
+        '7.0.1406': {
+          case $::provisioner {
+            'virtualbox': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
+            'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
+          }
+
+          $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
+          $udev_rule_gen    = '/lib/udev/rules.d/75-persistent-net-generator.rules'
+        }
+
         5.10, 5.11: {
           $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
