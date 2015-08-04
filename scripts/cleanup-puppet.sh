c182a245d9828617c09504f1c5b8dc22f1e0a55c
Heath Seals <heath@puppetlabs.com>
2015-08-04 19:38:18 +0200

    (maint) nocm box cleanup fixes
    The commit updates the logic used to cleanup nocm boxes.

--- scripts/cleanup-puppet.sh
+++ scripts/cleanup-puppet.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# PE is currently used to provision nocm and puppet boxes.
+# This script cleans up directories that may be left around
+# as part of that process.
+
 # Unmount NFS share if PUPPET_NFS provided
 if [ -n "${PUPPET_NFS}" ]; then
   umount -l /opt/puppet
 fi
 
-# Remove Puppet-related files and directories
-#rm -rf /etc/puppetlabs
+# Only remove /etc/puppetlabs on -nocm boxes
+if [[ "${PACKER_BUILD_NAME}" =~ .*-nocm ]]; then
+  rm -rf /etc/puppetlabs
+fi
+
+# Remove other Puppet-related files and directories
 rm -rf /opt/puppet
 rm -rf /var/cache/yum/puppetdeps
 rm -rf /var/opt/lib/pe-puppet
-rm -rf /var/opt/puppetlabs/puppet
+rm -rf /var/opt/puppetlabs
