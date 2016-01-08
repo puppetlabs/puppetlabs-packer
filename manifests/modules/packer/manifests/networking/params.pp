5a3f4bd9c882cb20d6e62b930046bfe1acad2a8d
Heath Seals <heath@puppetlabs.com>
2016-01-08 11:59:29 +0100

    (RE-5464) Fedora 23 networking params
    This commit adds Fedora 23 to the packer::networking module params.

--- manifests/modules/packer/manifests/networking/params.pp
+++ manifests/modules/packer/manifests/networking/params.pp
@@ -23,7 +23,7 @@ class packer::networking::params {
           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
         }
 
-        21, 22: {
+        21, 22, 23: {
           case $::provisioner {
             'virtualbox': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
             'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
