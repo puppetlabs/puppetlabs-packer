716ad72208d6cc98c65b2da8110cac416b063d33
Heath Seals <heath@puppetlabs.com>
2016-03-18 16:39:54 +0100

    (maint) switch module we use for networking fixups
    example42-network can be used for both debian and redhat based operating
    systems.

--- manifests/modules/packer/manifests/vsphere/networking.pp
+++ manifests/modules/packer/manifests/vsphere/networking.pp
@@ -1,16 +1,17 @@
 class packer::vsphere::networking inherits packer::networking::params {
 
   class { 'network':
-      config_file_notify => '',
+    config_file_notify => '',
   }
 
   case $::osfamily {
     debian: {
       if $::operatingsystemrelease in ['15.10', '16.04'] {
-        debnet::iface::loopback { 'lo': }
-        debnet::iface::dhcp { 'ens32': }
+        network::interface { 'ens32':
+          enable_dhcp   => true,
       }
     }
+  }
 
     redhat: {
       if ($::operatingsystem == 'Scientific') and ($::operatingsystemmajrelease == '7') {
