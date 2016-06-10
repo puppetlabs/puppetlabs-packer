21fedc460a35812a814a9e12214f12476d8b9097
John O'Connor <johnoconnor@Johns-MacBook-Pro-4.local>
2016-06-10 15:28:01 +0100

    (RE-7417) Remove bitvise completely
    Bitvise won't be used (at least in the initial packer implementation).
    So removing references and license file (it was already disabled).

--- templates/win-2012r2-x86_64/std/win-2012r2-x86_64-std.pp
+++ templates/win-2012r2-x86_64/std/win-2012r2-x86_64-std.pp
@@ -1,6 +1,3 @@
-#include bitvise_sshd
-#include bitvise_sshd::service
-
 include windows_template::local_group_policies
 include windows_template::disable_services
 include windows_template::disable_sounds
