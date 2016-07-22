846768a299e4a47eab47317f3f232a2fc309d9d2
jcoconnor <john.oconnor@puppet.com>
2016-07-22 16:50:45 +0100

    (RE-7457) Suppress sysinternal license popup
    Add registry entries to suppress the EULA popup on first use for the
    sysinternal utilities.
    
    Include entries for Default User and also puppet user.

--- scripts/windows/install-win-packages.ps1
+++ scripts/windows/install-win-packages.ps1
@@ -23,3 +23,44 @@ Write-Host "Installing Sysinternal Tools"
 chocolatey install procexp --yes --force
 chocolatey install procmon --yes --force
 chocolatey install pstools --yes --force
+
+# Put in registry keys to suppress the EULA popup on first use.
+# (since puppet modules don't support HKCU)
+# First a helper function
+
+function AcceptSysInternalsEULA {
+param (
+  [string]$regkeyroot
+ )
+   Write-Host "Setting Sysinternals Registry Keys for $regkeyroot"
+
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\Process Explorer" /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\Process Monitor"  /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsExec"           /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsFile"           /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsGetSid"         /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsInfo"           /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsKill"           /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsList"           /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsLoggedOn"       /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsLogList"        /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsPasswd"         /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsService"        /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsShutdown"       /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsSuspend"        /v EulaAccepted /t REG_DWORD /d 1 /f
+   reg.exe ADD "$regkeyroot\Software\Sysinternals\PsTools"          /v EulaAccepted /t REG_DWORD /d 1 /f
+}
+
+# Accept for current user.
+AcceptSysInternalsEULA HKCU
+
+# Same for the Default User
+reg.exe load HKLM\DEFUSER c:\users\default\ntuser.dat
+AcceptSysInternalsEULA HKLM\DEFUSER
+reg.exe unload HKLM\DEFUSER
+
+# Same for puppet User as profile already exists
+# Note - Realise this is not ideal, so will be tidied once we figure out how remove profiles.
+reg.exe load HKLM\PUPPET c:\users\puppet\ntuser.dat
+AcceptSysInternalsEULA HKLM\PUPPET
+reg.exe unload HKLM\PUPPET
