c83715bfeea062499f3e1cfe90f6749a8fdec0d6
jcoconnor <john.oconnor@puppet.com>
2016-07-27 09:50:52 +0100

    (RE-7519) Set IE Home Page to blank
    Set the default home page for current and default user.

--- templates/windows-2012r2/files/config-vmware-vsphere-cygwin.ps1
+++ templates/windows-2012r2/files/config-vmware-vsphere-cygwin.ps1
@@ -9,8 +9,18 @@ $ErrorActionPreference = "Stop"
 # Although this is no longer run under boxstarter, we are still able to use it's cmdlets.
 Write-Host "Other Stuff......."
 
+# Load Default User for registry to accomodate changes.
+reg.exe load HKLM\DEFUSER c:\users\default\ntuser.dat
+
 #Disable UAC for Windows-2012
 Disable-UAC
 
 # Enable Remote Desktop (with reduce authentication resetting here again)
 Enable-RemoteDesktop -DoNotRequireUserLevelAuthentication
+
+# Set IE Home Page for this and Default User.
+reg.exe ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d "about:blank" /f
+reg.exe ADD "HKLM\DEFUSER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d "about:blank" /f
+
+# Unload default user.
+reg.exe unload HKLM\DEFUSER
