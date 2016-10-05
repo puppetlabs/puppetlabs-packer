20648d42144b3b86742ff2524e37d78140463e0e
Johan De Wit <johan@koewacht.net>
2016-10-05 21:50:53 +0200

    adds centos 7 and some minor style correction

--- manifests/modules/packer/manifests/networking/params.pp
+++ manifests/modules/packer/manifests/networking/params.pp
@@ -10,10 +10,10 @@ class packer::networking::params {
       case $::operatingsystemrelease {
         '7.0.1406', '7.1.1503', '7.2.1511', '7.2': {
           case $::provisioner {
-            'virtualbox','libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
+            'virtualbox': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
             'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
+            'libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0' }
           }
-
           $udev_rule     = '/etc/udev/rules.d/70-persistent-net.rules'
           $udev_rule_gen = '/lib/udev/rules.d/75-persistent-net-generator.rules'
         }
