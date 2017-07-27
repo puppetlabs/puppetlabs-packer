c39745ba8430820587e30194cde5e6b883ad3931
John O'Connor <john.oconnor@puppet.com>
2017-07-27 14:48:08 +0100

    (IMAGES-573) Add Win-2016 Core Template
    This is a copy of the win-2012r2-core template with product keys and OS
    Version name updated for Windows-2016

---
+++ templates/windows-2016-core/files/x86_64/virtualbox/windows-2016-core-x86_64-virtualbox-base.package.ps1
@@ -0,0 +1,69 @@
+$ErrorActionPreference = "Stop"
+
+. A:\windows-env.ps1
+
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+if (Test-PendingReboot){ Invoke-Reboot }
+
+# Need to guard against system going into standby for long updates
+Write-BoxstarterMessage "Disabling Sleep timers"
+Disable-PC-Sleep
+
+if (-not (Test-Path "A:\Autologon.installed"))
+{
+  # Quick fix to get the autologon working for vagrant.
+  Write-BoxstarterMessage "Installing Autologon to get over sysprep login issues"
+  choco install autologon -y
+  reg.exe ADD "HKCU\Software\Sysinternals\Autologon" /v "EulaAccepted" /t REG_DWORD /d 1 /f
+  autologon vagrant . vagrant
+  Touch-File "A:\Autologon.installed"
+}
+
+if (-not (Test-Path "A:\NET462.installed"))
+{
+  # Install .Net Framework 4.5.2
+  Write-BoxstarterMessage "Installing .Net 4.6.2"
+  choco install dotnet-4.6.2 -y --install-arguments="/q /norestart"
+  Touch-File "A:\NET462.installed"
+  if (Test-PendingReboot) { Invoke-Reboot }
+}
+
+# Run the Packer Update Sequence
+Install-PackerWindowsUpdates
+
+# Disable UAC
+Write-BoxstarterMessage "Disable UAC"
+Disable-UAC
+
+# Enable Remote Desktop (with reduce authentication resetting here again)
+Write-BoxstarterMessage "Enable Remote Desktop"
+Enable-RemoteDesktop -DoNotRequireUserLevelAuthentication
+netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
+
+# Add WinRM Firewall Rule
+Write-BoxstarterMessage "Setting up winrm"
+netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+
+$enableArgs=@{Force=$true}
+try {
+ $command=Get-Command Enable-PSRemoting
+  if($command.Parameters.Keys -contains "skipnetworkprofilecheck"){
+      $enableArgs.skipnetworkprofilecheck=$true
+  }
+}
+catch {
+  $global:error.RemoveAt(0)
+}
+Enable-PSRemoting @enableArgs
+Enable-WSManCredSSP -Force -Role Server
+# NOTE - This is insecure but can be shored up in later customisation.  Required for Vagrant and other provisioning tools
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+Write-BoxstarterMessage "WinRM setup complete"
+
+# End
