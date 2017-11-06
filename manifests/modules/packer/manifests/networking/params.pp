a716dfc8ec5bacb49bdba531e549d93489896721
Jayant Sane <ghjsane@gmail.com>
2017-11-06 10:04:19 -0800

    (IMAGES-656) Create a RHEL7 FIPS mode packer image
    Created new rhel7 template with required scripts to configure it to
      - Enable repo's to be able install packages.
      - Boot in FIPS mode and
      - Install openssl package version aligned with that of openssl-devel
    
    Repo enabling is being done using a script which in future could be
    reworked to be done via puppet manifest (vsphere/repos.pp)
    
    	modified:   manifests/modules/packer/manifests/networking/params.pp
    	new file:   scripts/enable-rhel-repos.sh
    	new file:   scripts/enable_fips.sh
    	new file:   templates/rhel-7.2/calc_hash.rb
    	new file:   templates/rhel-7.2/files/x86_64.ks
    	new file:   templates/rhel-7.2/x86_64.vmware.base.json
    	new file:   templates/rhel-7.2/x86_64.vmware.vsphere.nocm.json

--- manifests/modules/packer/manifests/networking/params.pp
+++ manifests/modules/packer/manifests/networking/params.pp
@@ -7,38 +7,44 @@ class packer::networking::params {
     }
 
     redhat: {
-      case $::operatingsystemrelease {
-        '7.0.1406', '7.1.1503', '7.2.1511', '7.2': {
-          case $::provisioner {
-            'virtualbox','libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
-            'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
-          }
 
-          $udev_rule     = '/etc/udev/rules.d/70-persistent-net.rules'
-          $udev_rule_gen = '/lib/udev/rules.d/75-persistent-net-generator.rules'
-        }
+       case $::operatingsystemrelease {
+         '7.0.1406', '7.1.1503', '7.2.1511', '7.2': {
+           case $::provisioner {
+             'virtualbox','libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
+             'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33' }
+           }
+ 
+           $udev_rule     = '/etc/udev/rules.d/70-persistent-net.rules'
+           $udev_rule_gen = '/lib/udev/rules.d/75-persistent-net-generator.rules'
+         }
 
-        '5.11': {
-          $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
-          $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
-        }
-
-        '23', '24', '25', '26': {
-          case $::provisioner {
-            'virtualbox': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
-            'libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens4' }
-            'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens32' }
-          }
-          $udev_rule     = undef
-          $udev_rule_gen = undef
-        }
-
-        default: {
-          $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
-          $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
-          $udev_rule_gen    = '/lib/udev/rules.d/75-persistent-net-generator.rules'
-        }
-      }
+         '7.0': {
+           $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens33'
+           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
+         }
+ 
+         '5.11': {
+           $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
+           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
+         }
+ 
+         '23', '24', '25', '26': {
+           case $::provisioner {
+             'virtualbox': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-enp0s3' }
+             'libvirt': { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens4' }
+             'vmware':     { $interface_script = '/etc/sysconfig/network-scripts/ifcfg-ens32' }
+           }
+           $udev_rule     = undef
+           $udev_rule_gen = undef
+         }
+ 
+         default: {
+           $interface_script = '/etc/sysconfig/network-scripts/ifcfg-eth0'
+           $udev_rule        = '/etc/udev/rules.d/70-persistent-net.rules'
+           $udev_rule_gen    = '/lib/udev/rules.d/75-persistent-net-generator.rules'
+         }
+       }
     }
     suse: {
       $interface_script = '/etc/sysconfig/network/ifcfg-eth0'
