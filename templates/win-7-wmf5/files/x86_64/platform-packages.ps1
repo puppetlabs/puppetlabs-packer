7ce07f1f2e36a7739e7c46a20156acdd088c87fb
John O'Connor <john.oconnor@puppet.com>
2017-12-12 18:14:13 +0000

    (IMAGES-612) Add Win-7 WMF5 builds.
    Add WMF5 (Powershell 5) Windows 7 builds to the platform suite.

---
+++ templates/win-7-wmf5/files/x86_64/platform-packages.ps1
@@ -0,0 +1,23 @@
+$ErrorActionPreference = "Stop"
+
+. A:\windows-env.ps1
+
+Write-Host "Running Win-7 Package Customisation"
+
+if (-not (Test-Path "$PackerLogs\KB2852386.installed"))
+{
+  # Install the WinSxS cleanup patch
+  Write-Host "Installing Windows Update Cleanup Hotfix KB2852386"
+  Install_Win_Patch -PatchUrl "http://osmirror.delivery.puppetlabs.net/iso/windows/win-2008r2-msu/Windows6.1-KB2852386-v2-x64.msu"
+  Touch-File "$PackerLogs\KB2852386.installed"
+  if (Test-PendingReboot) { Invoke-Reboot }
+}
+
+if (-not (Test-Path "$PackerLogs\WMF5.installed"))
+{
+  # Install WMF 5 (Powershell)
+  Write-Host "Installing WFM 5.1"
+  Install_Win_Patch -PatchUrl "http://buildsources.delivery.puppetlabs.net/windows/wmf5/Win7AndW2K8R2-KB3191566-x64.msu"
+  Touch-File "$PackerLogs\WMF5.installed"
+  Invoke-Reboot
+}
