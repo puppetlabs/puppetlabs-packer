62c2a1bf1afd4f19cc4c1ac7dadce3d52a520930
Casey Williams <caseywilliams@users.noreply.github.com>
2018-03-22 12:49:26 -0700

    (IMAGES-752) Allow for Ubuntu 18.04 in packer modules
    Adds 18.04 as a valid release version alongside past versions.

--- manifests/modules/packer/manifests/vmtools.pp
+++ manifests/modules/packer/manifests/vmtools.pp
@@ -2,7 +2,7 @@ class packer::vmtools inherits packer::vmtools::params {
 
   case $::osfamily {
     debian: {
-      if ($::operatingsystemmajrelease in ['7', '8', '9', '16.04']) {
+      if ($::operatingsystemmajrelease in ['7', '8', '9', '16.04', '18.04']) {
         package { 'open-vm-tools':
           ensure => installed,
         }
