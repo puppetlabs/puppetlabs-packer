0d4fde75d40fac806b57aa9c4eab9053347c2939
John O'Connor <john.oconnor@puppet.com>
2018-05-01 16:51:40 +0100

    Minor fix

--- templates/win/10-ent/x86_64/files/platform-packages.ps1
+++ templates/win/10-ent/x86_64/files/platform-packages.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 . C:\Packer\Scripts\windows-env.ps1
 
-Write-Host "Running Win-10 Package Customisationtemplates/windows-10/files/i386/platform-packages.ps1"
+Write-Host 'Running Win-10 Package Customisation templates/windows-10/files/i386/platform-packages.ps1'
 
 # Flag to remove Apps packages and other nuisances
 Touch-File "$PackerLogs\AppsPackageRemove.Required"
