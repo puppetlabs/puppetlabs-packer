f8dbe948ac366642d0f55f8d3a97e1487c55bbeb
John O'Connor <john.oconnor@puppet.com>
2018-10-26 10:55:34 +0100

    (IMAGES-975) WindowsUpdate Tests
    Add Smoke tests to verify that WindowsUpdate has happened including
    1. Windows Has run
    2. Updates have been installed.
    3. Most recent cumulative update (for windows-10/2016)

---
+++ templates/win/common/scripts/acceptance/windows-update/WindowsUpdate.Tests.ps1
@@ -0,0 +1,27 @@
+<#
+  .SYNOPSIS
+	Windows Update Tests
+  .DESCRIPTION
+    A set of tests that PsWindowsUpdate has run and actual
+    windows updates are installed.
+#>
+
+#
+
+. C:\Packer\Scripts\windows-env.ps1
+
+describe 'Windows Update Validation Tests' {
+
+    it 'Should Have at least One Windows Update installed' {
+    }
+
+    it 'The latest windows update should have been installed today' {
+    }
+
+    it 'Should have installed a Cumulative update' {
+
+    }
+
+    it 'Should not have any Windows Updates Pending'
+
+}
