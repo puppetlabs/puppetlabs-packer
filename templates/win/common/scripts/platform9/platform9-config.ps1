6ed8635239c64f3b09ae79c03bf53ab9ff9974d0
John O'Connor <john.oconnor@puppet.com>
2019-01-21 15:57:25 +0000

    (IMAGES-1004) Platform9 2 stage build
    Create two separate vcenter build files initially for Platform9.
    Although platform9 is a bios build, the base file should be generic
    enough to build either UEFI or BIOS files (little packer difference
    between them once the disk format is setup in the autounattend.xml
    file).
    
    The base file creates an up to date windows base image, whereas the
    platform9 file performs the necessary base configuration. Further
    work will be done in later tickets to add the correct drivers and
    sysprep stage for platform9.
    
    Provide placeholder platform9 script file to verify the build.

---
+++ templates/win/common/scripts/platform9/platform9-config.ps1
@@ -0,0 +1,11 @@
+# Platform9 Configuration Script.
+# Placeholder for Platform 9 scripts.
+
+. C:\Packer\Scripts\windows-env.ps1
+
+$rundate = date
+write-output "Script: platform9-config.ps1 Starting at: $rundate"
+
+
+
+Write-Output "Bye"
