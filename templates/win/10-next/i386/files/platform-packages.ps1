d5fe03c591806e09da0f010f368b1fda9fd2a7e7
John O'Connor <john.oconnor@puppet.com>
2019-06-11 08:29:48 +0100

    (IMAGES-1117) Add Win 10 1903 and 2019 LTSC
    Adding the Windows 10 1903 build as win-10-next and the 2019 LTSC build
    as win-10-1809

---
+++ templates/win/10-next/i386/files/platform-packages.ps1
@@ -0,0 +1,7 @@
+
+. C:\Packer\Scripts\windows-env.ps1
+
+Write-Output "Running Win-10 Package Customisation"
+
+# Flag to remove Apps packages and other nuisances
+Touch-File "$PackerLogs\AppsPackageRemove.Required"
