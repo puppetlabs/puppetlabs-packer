8314c906c7bed5259d21eca21379dc10cb0718d5
John O'Connor <john.oconnor@puppet.com>
2019-09-26 14:04:27 +0100

    (IMAGES-845) Set Secpol using forge module
    Using the forge module: ayohrling/local_security_policy to set the
    security policies in puppet instead of applying a policy file.

--- templates/win/common/scripts/provisioners/config-winsettings.ps1
+++ templates/win/common/scripts/provisioners/config-winsettings.ps1
@@ -7,10 +7,6 @@
 Write-Output "Enable Bootlog"
 cmd /c "bcdedit /set {current} bootlog yes"
 
-# Set the Security Policies
-Write-Output "Setting Low Security Password Policies"
-secedit /configure /db secedit.sdb /cfg $PackerConfig\Low-SecurityPasswordPolicy.inf /quiet
-
 # Re-Enable AutoAdminLogon
 autologon -AcceptEula Administrator . PackerAdmin
 
