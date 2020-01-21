09a6467bf403be8f76234269e41ab249c2bf60ab
John O'Connor <john.oconnor@puppet.com>
2020-01-21 10:32:12 +0000

    (IMAGES-1174) Service Stack Updates
    As happens, there are new Servicing Stack Updates that need to be
    applied to allow the Update process to run smoothly.

--- templates/win/10-ent/i386/files/platform-packages.ps1
+++ templates/win/10-ent/i386/files/platform-packages.ps1
@@ -5,3 +5,11 @@ Write-Output "Running Win-10 Package Customisation"
 
 # Flag to remove Apps packages and other nuisances
 Touch-File "$PackerLogs\AppsPackageRemove.Required"
+
+if (-not (Test-Path "$PackerLogs\KB4528759.installed"))
+{
+  Write-Output "Installing Windows Update SSU KB4528759"
+  Install_Win_Patch -PatchUrl "http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/01/windows10.0-kb4528759-x86_5ee4c5443a4d70dcf2399e1c2afe57f625e72b3d.msu"
+  Touch-File "$PackerLogs\KB4528759.installed"
+  Invoke-Reboot
+}
--- templates/win/10-pro/x86_64/files/platform-packages.ps1
+++ templates/win/10-pro/x86_64/files/platform-packages.ps1
@@ -5,3 +5,11 @@ Write-Output "Running Win-10 Package Customisationtemplates/windows-10/files/i38
 
 # Flag to remove Apps packages and other nuisances
 Touch-File "$PackerLogs\AppsPackageRemove.Required"
+
+if (-not (Test-Path "$PackerLogs\KB4528759.installed"))
+{
+  Write-Output "Installing Windows Update SSU KB4528759"
+  Install_Win_Patch -PatchUrl "http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/01/windows10.0-kb4528759-x64_c2d6639977986b927d0b9f1acf0fb203c38fc8c8.msu"
+  Touch-File "$PackerLogs\KB4528759.installed"
+  Invoke-Reboot
+}
