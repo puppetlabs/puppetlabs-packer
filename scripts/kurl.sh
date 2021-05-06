b692f5b0522c5ed44391e6eeec17e7bf458e6277
Josh Partlow <joshua.partlow@puppetlabs.com>
2021-05-06 23:54:36 +0000

    (REPLATS-180) Remove kurl image installer patch
    The puppet-application-manager-standalone build doesn't contain
    prometheus. If future builds need to patch the installer, they can
    search the git history for how.

--- scripts/kurl.sh
+++ scripts/kurl.sh
@@ -24,16 +24,5 @@ curl -sSLO https://k8s.kurl.sh/bundle/${APP}-${CHANNEL}.tar.gz
 tar xzf ${APP}-${CHANNEL}.tar.gz
 rm ${APP}-${CHANNEL}.tar.gz
 
-cat << EOF > patch.yaml
-apiVersion: cluster.kurl.sh/v1beta1
-kind: Installer
-metadata:
-  name: patch
-spec:
-  # Disable Prometheus. Not necessary for testing and reduces resource requirements.
-  prometheus:
-    version: ''
-EOF
-
 dnf -y install bash-completion
-cat install.sh | sudo bash -s airgap preserve-selinux-config installer-spec-file=patch.yaml
+cat install.sh | sudo bash -s airgap preserve-selinux-config
