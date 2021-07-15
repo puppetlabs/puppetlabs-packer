ad32585d0020f4951132de85b7f9595fc1af46c9
Josh Partlow <joshua.partlow@puppetlabs.com>
2021-07-15 20:42:47 +0000

    (REPLATS-348) Only preload kurl in the 8.3-kurl-ha image
    Restarting k8s was challenging for a standalone primary. It's likely to
    be worse for an ha primary with ceph/rook, and then we'd need to worry
    about joining other primaries to the cluster for ha.
    
    Instead, we're just going to pre-download the kurl tarball, which should
    be the bulk of the time, and then install fresh on checked out vms.
    
    Curl's puppet-application-manager-stable instead of standalone, and
    updates the kurl.sh to skip installation and managing a restart script
    (which we aren't copying into the image) if we are not installing.

--- scripts/kurl.sh
+++ scripts/kurl.sh
@@ -3,26 +3,33 @@
 # These should be passed in via custom_provisioning_env
 #APP='puppet-application-manager'
 #CHANNEL='standalone'
+#INSTALL=true (default, set false to to skip installation)
+INSTALL="${INSTALL:-true}"
 
 set -e
 
 #################################################################################
 # Provide a script to get kubernetes back into a working state after vm checkout.
 
-sudo mv /tmp/restart_k8s.sh /root
-sudo chmod 750 /root/restart_k8s.sh
+if [ "${INSTALL}" == 'true' ]; then
+  sudo mv /tmp/restart_k8s.sh /root
+  sudo chmod 750 /root/restart_k8s.sh
 
-cat << EOF | sudo tee -a /etc/motd
+  cat << EOF | sudo tee -a /etc/motd
 
 After initial checkout, run /root/restart_k8s.sh to get the Kubernetes environment restarted.
 
 EOF
 
+fi
+
 #############################
 # Install Kubernetes via Kurl
-curl -sSLO https://k8s.kurl.sh/bundle/${APP}-${CHANNEL}.tar.gz
-tar xzf ${APP}-${CHANNEL}.tar.gz
-rm ${APP}-${CHANNEL}.tar.gz
+curl -sSLO "https://k8s.kurl.sh/bundle/${APP}-${CHANNEL}.tar.gz"
+tar xzf "${APP}-${CHANNEL}.tar.gz"
+rm "${APP}-${CHANNEL}.tar.gz"
 
 dnf -y install bash-completion
-cat install.sh | sudo bash -s airgap preserve-selinux-config
+if [ "${INSTALL}" == 'true' ]; then
+  cat install.sh | sudo bash -s airgap preserve-selinux-config
+fi
