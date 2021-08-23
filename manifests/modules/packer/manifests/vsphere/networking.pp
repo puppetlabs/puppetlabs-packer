5434ca40d3c2c023ddf5283855470987e5b26102
Dorin Pleava <dorin.pleava@puppet.com>
2021-08-23 13:31:46 +0300

    (IMAGES-1290) Update Debian 11 to use realeased iso

--- manifests/modules/packer/manifests/vsphere/networking.pp
+++ manifests/modules/packer/manifests/vsphere/networking.pp
@@ -28,7 +28,7 @@ class packer::vsphere::networking(
           enable_dhcp   => true,
         }
       }
-      if $facts['operatingsystemmajrelease'] in ['10', 'bullseye/sid'] {
+      if $facts['operatingsystemmajrelease'] in ['10', '11'] {
         network::interface { 'ens192':
           enable_dhcp   => true,
         }
