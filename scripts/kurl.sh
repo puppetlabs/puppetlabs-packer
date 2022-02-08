6458928c57ee1b7d9f2e754eef53211dded04e95
Josh Partlow <joshua.partlow@puppetlabs.com>
2022-02-08 21:40:44 +0000

    (maint) Remove SC2002 complaint from kurl-beta install script
    Have had trouble getting this to stick in the past, possibly because of
    the sudo. Trying again with a -E to preserve environment.

--- scripts/kurl.sh
+++ scripts/kurl.sh
@@ -19,7 +19,7 @@ curl -sSLO "https://k8s.kurl.sh/bundle/${APP}-${CHANNEL}.tar.gz"
 tar xzf "${APP}-${CHANNEL}.tar.gz"
 rm "${APP}-${CHANNEL}.tar.gz"
 
-cat install.sh | sudo bash -s airgap preserve-selinux-config
+sudo -E bash install.sh airgap preserve-selinux-config
 
 # Stop pods and Kubelet before shutdown. The packer build fills the disk with 0s to compress the
 # image, which otherwise causes Kubelet to start erasing unused images that we still need. Other
